2444c045d7085d1b7500454d1d90b4582ebd426c
Flávio Júnior <59364674+flavio-junior@users.noreply.github.com>
2024-07-12 08:43:10 -0300

    script with command to execute programs

---
+++ launch-programs.bat
@@ -0,0 +1,27 @@
+@ECHO OFF
+
+CD "C:\Program Files\Google\Chrome\Application"
+start "C:\Program Files\Google\Chrome\Application"
+
+CD "C:\Users\flavi\AppData\Local\Programs\Android Studio\bin"
+Start studio64.exe
+
+CD "C:\Users\flavi\AppData\Local\Programs\IntelliJ IDEA Community Edition\bin"
+start idea64.exe
+
+CD "C:\Users\flavi\AppData\Local\Postman"
+Start Postman.exe
+
+CD "C:\Program Files\MySQL\MySQL Workbench 8.0"
+Start MySQLWorkbench.exe
+
+CD "C:\Program Files\Notepad++"
+Start notepad++.exe
+
+CD "C:\Windows\system32"
+start cmd.exe
+
+CD "C:\Windows\System32"
+start notepad.exe
+
+Exit
