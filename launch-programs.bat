78f5eaa7d5e7964e2705d587c78ecc14bc53d9e6
Flávio Júnior <59364674+flavio-junior@users.noreply.github.com>
2024-07-15 08:46:49 -0300

    Update launch-programs.bat

--- launch-programs.bat
+++ launch-programs.bat
@@ -7,10 +7,13 @@ CD "C:\Windows\system32"
 start cmd.exe
 
 CD "C:\Users\flavi\AppData\Local\Programs\Android Studio\bin"
-Start studio64.exe
+Start studio64.exe E:\developer\android-compose\hello-world
 
 CD "C:\Users\flavi\AppData\Local\Programs\IntelliJ IDEA Community Edition\bin"
-start idea64.exe
+start idea64.exe E:\developer\spring-boot\hello-world
+timeout /t 10
+CD /d E:\developer\spring-boot\hello-world
+java -jar target\hello-world-0.0.1-SNAPSHOT.jar
 
 CD "C:\Users\flavi\AppData\Local\Postman"
 Start Postman.exe
